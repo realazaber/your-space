474abc25fc3cdeb3f4f2a3b57ea5808208b8f967
swagoverlord <azcooldude8@gmail.com>
2023-05-22 19:03:36 +0200

    Base user entity

---
+++ backend/Entities/User.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace backend.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}

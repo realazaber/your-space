bda0a51fd6e43d376c93bded924be7b28a4b6479
swagoverlord <azcooldude8@gmail.com>
2023-05-26 23:51:02 +0200

    Setup entity framework

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/weather")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -17,8 +17,6 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
-
-    [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -29,4 +27,6 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,15 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 

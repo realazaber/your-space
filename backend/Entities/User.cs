f733824053615d1d49d9af9e47cad4f6ef454114
therealcoolpup <azcooldude8@gmail.com>
2023-06-04 18:27:41 +0200

    Added password hash fields to user

--- backend/Entities/User.cs
+++ backend/Entities/User.cs
@@ -10,6 +10,8 @@ namespace backend.Entities
 
         public string Email { get; set; }
 
-        public string Password { get; set; }
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }

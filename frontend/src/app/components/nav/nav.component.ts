afd766c475c695d92058d88dfe08b2db518db66a
therealcoolpup <azcooldude8@gmail.com>
2023-06-13 14:44:50 +0200

    Base nav

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgMaterialModule } from 'src/app/modules/ng-material/ng-material.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgMaterialModule, RouterModule],
   templateUrl: './nav.component.html',
 })
 export class NavComponent {}
